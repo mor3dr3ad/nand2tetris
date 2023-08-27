625bdc03b5b679b30bef3830529a86f8a8f8b0f5
mor3dr3ad <christof.weigelmeier@posteo.net>
2023-08-27 12:12:23 +0200

    Commit after working assembler with labels

--- projects/06/main.py
+++ projects/06/main.py
@@ -5,6 +5,7 @@ also uses code for support
 
 from parser import Parser
 from coder import Code
+from symbolHandler import symbolHandler
 
 import os
 import sys
@@ -14,7 +15,7 @@ class Main:
     """Main class."""
 
     def main():
-        """Main function."""
+        """Run the program."""
         # first check cli args
         if len(sys.argv) > 1:
             filename = os.path.join(os.getcwd(), sys.argv[1])
@@ -23,18 +24,47 @@ class Main:
         # initialize parser
         parser = Parser(filename)
 
+        # initialize symbol class
+        symbolhandler = symbolHandler()
+
+        # first pass: run through commands,
+        # count value of command and
+        # insert into symbol table for each label
+        counter = 0
+        while parser.hasMoreCommands():
+            parser.advance()
+            if parser.commandType() == "L_Command":
+                # add to symbolTable
+                symbolhandler.insertLabel(parser.symbol(), counter)
+            else:
+                counter += 1
+        # need to reset parser somehow,
+        parser.resetParser()
+
         hack_filename = filename.replace('asm', 'hack')
         hack_file = open(hack_filename, 'w')
 
+        code = Code()
+        addressCounter = 16
         while parser.hasMoreCommands():
             parser.advance()
             # command = ''
-            code = Code()
+            # A Command can be 3 things:
+            # - a number
+            # - a known symbol or 
+            # - a new symbol
             if parser.commandType() == 'A_Command':
-                # get symbol and translate
-                symbol = int(parser.symbol())
+                symbol = parser.symbol()
+                if symbol.isdecimal():
+                    num = int(symbol)
+                elif symbol in symbolhandler.symbolTable:
+                    num = symbolhandler.symbolTable[symbol]
+                else:
+                    num = addressCounter
+                    symbolhandler.insertLabel(symbol, addressCounter)
+                    addressCounter += 1
                 # translate number to binary representation
-                hack_file.write(format(symbol, "016b"))
+                hack_file.write(format(num, "016b"))
                 hack_file.write("\n")
             elif parser.commandType() == 'C_Command':
                 # get dest,comp,jump and translate
@@ -44,7 +74,6 @@ class Main:
                 # reassemble into binary
                 hack_file.write("111" + comp + dest + jump)
                 hack_file.write("\n")
-
             else:
                 pass
 
--- projects/06/parser.py
+++ projects/06/parser.py
@@ -1,15 +1,16 @@
 """Parses the given command list."""
+import re
 
 
 class Parser:
+    """Parse the file and return the parts of the command for processing."""
 
     def __init__(self, filename):
-        """
-        initializes the function
-        """
+        """Initialize the function."""
         with open(filename, 'r', encoding='utf-8') as file:
             self.commands = file.readlines()
             self.cleanCommands = []
+            # cleaning logic also works for labels
             for command in self.commands:
                 cleanCommand = ''.join(command.split('//')[0].split())
                 if cleanCommand != '':
@@ -17,24 +18,26 @@ class Parser:
         self.counter = -1
         self.currentCommand = ''
 
+    def resetParser(self):
+        """Reset the Parser to start over (first/second pass)."""
+        self.counter = -1
+        self.currentCommand = ''
+
     def hasMoreCommands(self):
-        """
-        function to determine if there are
-        any more commands in the processed data
-        """
+        """Determine if there more commands in the processed data."""
         return self.counter + 1 < len(self.cleanCommands)
 
     def advance(self):
-        """
-        function to move on to the next token.
+        """Move on to the next token.
+
         does not return anything but makes next command the current
         """
         self.counter += 1
         self.currentCommand = self.cleanCommands[self.counter]
 
     def commandType(self):
-        """
-        function to determine the type of command:
+        """Determine the type of command.
+
         - A_Command for @xxx where xxx is either symbol or decimal
         - B_Command for dest=comp;jump
         - L_Command (actually pseudocommand) for (Xxx) where Xxx is a symbol
@@ -48,17 +51,19 @@ class Parser:
         return 'C_Command'
 
     def symbol(self):
-        """
-        returns the symbol 'xxx' or decimal of the current command @xxx.
+        """Return the symbol 'xxx' or decimal of the current command @xxx.
+
         only called when command is A_Command or L_Command
         """
         if self.commandType() == 'A_Command':
             return self.currentCommand.split('@')[1]
+        elif self.commandType() == 'L_Command':
+            return re.search(r'\((.*?)\)', self.currentCommand).group(1)
         return ""
 
     def dest(self):
-        """
-        returns dest mnemoic in the current C-command (8 possibilities).
+        """Return dest mnemoic in the current C-command (8 possibilities).
+
         Only called with C_Command reminder C_command: dest=comp; jump
         """
         if self.commandType() == 'C_Command':
@@ -67,8 +72,8 @@ class Parser:
         return ""
 
     def comp(self):
-        """
-        returns the /comp/ mnemonic in the current C-command (28 possibilities)
+        """Return the mnemonic in the current C-command.
+
         Should be called only with C_Command. C_command: dest=comp; jump
         """
         print(self.currentCommand)
@@ -80,8 +85,8 @@ class Parser:
         return ""
 
     def jump(self):
-        """
-        returns the jump mnemoic in the current C-command (8 possibilities)
+        """Return the jump mnemoic in the current C-command.
+
         Should be called only with C_Command
         """
         if self.commandType() == 'C_Command':
